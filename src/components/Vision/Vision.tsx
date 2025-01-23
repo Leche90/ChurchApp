88160695e7928ea8dbde4e3c4634411f08275868
Leche90 <oliverleche9@gmail.com>
2025-01-23 10:56:19 -0600

    updated vision

--- src/components/Vision/Vision.tsx
+++ src/components/Vision/Vision.tsx
@@ -4,11 +4,17 @@ import './Vision.css'
 const Vision: React.FC = () => {
   return (
     <div className='vision-container'>
-            <h3 className='vision'>Vision</h3>
-            <p className='vision-text'>
-                To be a beacon of hope and faith in Winnipeg, Canada, and to inspire others to live a life of love, peace, and unity.
-            </p>
-        </div>
+      <h3 className='vision'>VISION</h3>
+        <span className="letter">V</span>
+        <span className="letter">I</span>
+        <span className="letter">S</span>
+        <span className="letter">I</span>
+        <span className="letter">O</span>
+        <span className="letter">N</span>
+        <p className='vision-text'>
+            To be a beacon of hope and faith in Winnipeg, Canada, and to inspire others to live a life of love, peace, and unity.
+        </p>
+    </div>
   )
 }
 
