65b99c332f9b7833060aa8b938fc0b461f910284
Leche90 <oliverleche9@gmail.com>
2025-01-23 12:33:19 -0600

    updated core values

--- src/components/Values/Values.tsx
+++ src/components/Values/Values.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import './Values.css'
 
-const Values = () => {
-  return (
-    <div>Values</div>
-  )
+const Values: React.FC = () => {
+    return (
+      <div className='values-container'>
+        <div className='values'>CORE VALUES</div>       
+          <p className='values-text'>
+              <ul>
+                <ol>1. Righteousness</ol>
+                <ol>2. Prayer</ol>
+                <ol>3. Royalty</ol>
+                <ol>4. Excellence</ol>
+                <ol>5. Accountability</ol>
+                <ol>6. Love</ol>
+                <ol>7. Family</ol>
+              </ul>
+          </p>
+        </div>
+    )
 }
 
 export default Values
+
