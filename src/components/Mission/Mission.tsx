5927cb85fa0e12505c1593cb051abf1eec126919
Leche90 <oliverleche9@gmail.com>
2025-01-23 12:06:15 -0600

    updated mission

--- src/components/Mission/Mission.tsx
+++ src/components/Mission/Mission.tsx
@@ -4,7 +4,7 @@ import './Mission.css'
 const Mission: React.FC = () => {
   return (
     <div className='mission-container'>
-        <div className='mission'>Mission</div>
+        <div className='mission'>MISSION</div>
         <p className='mission-text'>
             To reach out, discover, train and release members to be like Jesus wherever we are.
         </p>
