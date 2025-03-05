ec4941bb6d6011c7040330eaaadb89f97bf25e8d
Leche90 <oliverleche9@gmail.com>
2025-03-05 10:33:02 -0600

    updated backend models

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,10 @@
 import express, { Request, Response } from 'express';
 import formRoutes from './routes/form';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
 
 const app = express();
 
@@ -14,7 +19,14 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Welcome to the Church Backend API');
 });
 
-// Set up port
+// MongoDB Connection
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/churchDB';
+
+mongoose.connect(MONGO_URI)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error("MongoDB Conection Error:", err));
+
+    // Set up port
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
--- backend/src/models/formModel.ts
+++ backend/src/models/formModel.ts
@@ -1,10 +1,26 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
 // Placeholder for MongoDB schema
-export interface FormData {
+export interface IForm extends Document {
   firstName: string;
   lastName: string;
+  userName: string;
   email: string;
   phone: string;
   address: string;
   country: string;
   password: string;
 }
+
+const FormSchema: Schema = new Schema ({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  userName: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  phone: { type: String, required: true },
+  address: { type: String, required: true },
+  country: { type: String, required: true },
+  password: { type: String, required: true }
+}, { timestamps: true });
+
+export default mongoose.model<IForm>('Form', FormSchema);
