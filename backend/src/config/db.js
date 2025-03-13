4f8d2f911090f899e622ba76ddf7ef50442824fb
Leche90 <oliverleche9@gmail.com>
2025-03-13 00:39:17 -0500

    updated backend

---
+++ backend/src/config/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URI)
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error('Error connecting to MongoDB:', err);
+    process.exit(1); // Exit the process if the connection fails
+  }
+};
+
+module.exports = connectDB;
