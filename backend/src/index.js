91ad9bd4a40bf7a282999d10dccb22cfaea8b3c2
Leche90 <oliverleche9@gmail.com>
2025-03-13 15:54:20 -0500

    updated backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
+const mongoose = require('mongoose'); 
 
 // Connect to MongoDB
 connectDB();
@@ -12,6 +13,17 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({ origin: 'http://localhost:5173' }));
+
+// MongoDB connection (using Mongoose)
+const dbURI = process.env.MONGO_URI || 'mongodb+srv://KIMWebApp:KMIMPassApp2025@kimapp-cluster.vwfo8.mongodb.net/churchApp';
+mongoose.connect(dbURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('MongoDB connected');
+}).catch((err) => {
+  console.log('Error connecting to MongoDB:', err);
+});
 
 // Routes
 app.use('/api/auth', authRoutes);
